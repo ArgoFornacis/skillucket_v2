6b1fa4b6002397e7a721a54b44e396e83d0cf1a9
Barak Shalom <118911734+BarakMShalom@users.noreply.github.com>
2023-09-06 09:15:48 +0200

    Create Skill model

---
+++ project_v2/skillucketApp/constants.py
@@ -0,0 +1,9 @@
+CATEGORY_CHOICES = [
+    ('language', 'Language'),
+    ('music', 'Music'),
+    ('general_life_skills', 'General Life Skills'),
+    ('hobbies_and_crafts', 'Hobbies and Crafts'),
+    ('visual_arts', 'Visual Arts'),
+    ('programming', 'Programming'),
+    ('crime_and_exploitation', 'Crime and Exploitation'),
+]
---
+++ project_v2/skillucketApp/models/skill.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+from skillucket_v2.project_v2.skillucketApp.constants import CATEGORY_CHOICES  # constants.py contains model constants for more modularity and flexibility
+
+
+class Skill(models.Model):
+    """Represents a skill that can be acquired or learned by a user.
+
+    Skills are categorized into different categories such as 'Language', 'Music',
+    'General Life Skills', etc. Each skill has a name, category, description, and
+    can optionally be associated with a user.
+    Example:
+        skill = Skill(name="Python Programming", category="Programming",
+                      description="Learn Python programming language")
+    """
+
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
+    description = models.TextField(default="No description available")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
