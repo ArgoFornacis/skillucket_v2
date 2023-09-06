3951e32bd513b4e9a7f7b2bbbde9a011bc78000a
mariana <dragomir.mariana@web.de>
2023-09-06 13:42:13 +0200

    base html created

--- project_v2/skillucketApp/views.py
+++ project_v2/skillucketApp/views.py
@@ -1,24 +1,13 @@
 
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# def home_view(request):
-#
-#     return render(request, 'skillucketApp/home.html')
-#
-# def profile_view(request):
-#
-#     return render(request, 'skillucketApp/profile.html')
-
-
-from django.http import HttpResponse
 
 def home_view(request):
 
-    html_content = "/home"
-    return HttpResponse(html_content)
+    return render(request, 'home.html')
 
 def profile_view(request):
 
-    html_content = "/profile"
-    return HttpResponse(html_content)
+    return render(request, 'profile.html')
+
+
+
