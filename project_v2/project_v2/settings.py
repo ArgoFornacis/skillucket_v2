c74ee2148527a2c5fef3ede48e8e2ce1bf286815
mariana <dragomir.mariana@web.de>
2023-09-06 12:17:34 +0200

    templates update

--- project_v2/project_v2/settings.py
+++ project_v2/project_v2/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
--- project_v2/skillucketApp/views.py
+++ project_v2/skillucketApp/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# def home_view(request):
+#
+#     return render(request, 'skillucketApp/home.html')
+#
+# def profile_view(request):
+#
+#     return render(request, 'skillucketApp/profile.html')
+
+
+from django.http import HttpResponse
+
+def home_view(request):
+
+    html_content = "/home"
+    return HttpResponse(html_content)
+
+def profile_view(request):
+
+    html_content = "/profile"
+    return HttpResponse(html_content)
