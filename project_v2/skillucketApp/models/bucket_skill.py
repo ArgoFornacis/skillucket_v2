d852ec3886bb3b7087ba7ec5cfbb663e53faa751
ArgoFornacis <roza.wadowska@gmail.com>
2023-09-06 11:23:53 +0200

    changes day 1

--- project_v2/skillucketApp/models/bucket_skill.py
+++ project_v2/skillucketApp/models/bucket_skill.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from skillucketApp.models.skill import Skill
 
 
-class UserSkill(models.Model):
+class BucketSkill(models.Model):
     """ bucket skill model represents the skills that a specific user wants to acquire """
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)  # import Skill
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
     date_added = models.DateField(auto_now_add=True)
     target_date = models.DateField(null=True, blank=True)  # do we want this field?
 
--- project_v2/skillucketApp/models/user_skill.py
+++ project_v2/skillucketApp/models/user_skill.py
@@ -1,17 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from skillucketApp.constants import PROFICIENCY_CHOICES
+from skillucketApp.models.skill import Skill
 
 
 class UserSkill(models.Model):
     """ user skill model represents the skills that a specific user have """
 
-    PROFICIENCY_CHOICES = [
-        ('beginner', 'Beginner'),
-        ('intermediate', 'Intermediate'),
-        ('advanced', 'Advanced'),
-        ('expert', 'Expert'),
-    ]
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)  # import Skill
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
     proficiency_level = models.CharField(choices=PROFICIENCY_CHOICES)
 
+    def __str__(self):
+        return f"{self.user.username} knows: {self.skill.name}"
