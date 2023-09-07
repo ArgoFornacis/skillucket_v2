8d517290334f24bd0258a6187e774287ece9652c
ArgoFornacis <roza.wadowska@gmail.com>
2023-09-07 09:23:18 +0200

    minor change

--- project_v2/skillucketApp/urls.py
+++ project_v2/skillucketApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home_view, name='home'),
-    path('profile', views.profile_view, name='profile'),
+    path('home/', views.home_view, name='home'),
+    path('profile/', views.profile_view, name='profile'),
 ]
