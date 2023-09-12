c8363cbb306542c4eb0e4e7ea68805a045c24f27
mariana <dragomir.mariana@web.de>
2023-09-12 15:51:40 +0200

    more templates added

--- project_v2/skillucketContactForm/urls.py
+++ project_v2/skillucketContactForm/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("contact/", views.contact, name="contact"),
-    path("contact/success/", views.contact_success, name="contact_success"),
+    path("", views.contact, name="contact"),
+    path("success/", views.contact_success, name="success"),
 ]
