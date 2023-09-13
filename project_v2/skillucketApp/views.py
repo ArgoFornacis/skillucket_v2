7e16ba8b1eb7235cf9ddfcccd9538a1e1d349e0a
mariana <dragomir.mariana@web.de>
2023-09-13 15:01:51 +0200

    some changes

--- project_v2/skillucketApp/views.py
+++ project_v2/skillucketApp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 
 def home_view(request):
 
@@ -9,3 +11,22 @@ def home_view(request):
 def profile_view(request):
 
     return render(request, "profile.html")
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Welcome, {user.username}!")
+                return redirect('home')
+            else:
+                messages.error(request, "Invalid username or password.")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
