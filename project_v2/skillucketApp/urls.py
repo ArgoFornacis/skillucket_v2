71b6768ea04e266ef7a4b63d8fbbca8c10fad9f7
mariana <dragomir.mariana@web.de>
2023-09-13 15:56:50 +0200

    logout view

--- project_v2/skillucketApp/urls.py
+++ project_v2/skillucketApp/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("home", views.home_view, name="home"),
     path("profile", views.profile_view, name="profile"),
     path('login/', views.user_login, name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('password_change/', PasswordChangeView.as_view(success_url=reverse_lazy('password_change_done')), name='password_change'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
--- project_v2/skillucketApp/views.py
+++ project_v2/skillucketApp/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.forms import PasswordChangeForm
+from django.urls import reverse_lazy
+from django.contrib.auth import logout
 
 def home_view(request):
 
@@ -30,3 +34,28 @@ def user_login(request):
         form = AuthenticationForm()
 
     return render(request, 'login.html', {'form': form})
+
+
+
+
+
+def custom_logout(request):
+    logout(request)
+    return redirect('home')
+
+
+def custom_password_change(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Log the user in again (optional)
+            login(request, user)
+            messages.success(request, 'Your password was successfully changed.')
+            return redirect('profile')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+
+    return render(request, 'password_change.html', {'form': form})
